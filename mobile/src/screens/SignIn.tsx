85f7cee51b1a7ae437205df3c13f087322f09057
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2022-11-21 20:17:14 -0300

    create componente button

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,27 @@
-import { Center, Text } from 'native-base';
+import { Center, Text, Icon } from 'native-base';
+import { Fontisto } from '@expo/vector-icons';
+
+import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
 
 export function SignIn() {
     return (
-        <Center flex={1} bgColor="gray.900">
-            <Text color="white" fontSize={24}>
-                SignIn
+        <Center flex={1} bgColor="gray.900" p={7}>
+            <Logo width={212} height={40} />
+
+            <Button
+                type="SECONDARY"
+                title="ENTRAR COM GOOGLE"
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+                mt={12}
+            />
+
+            <Text
+                mt={4}
+                color="white"
+                textAlign="center"
+            >
+                Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
             </Text>
         </Center>
     );
