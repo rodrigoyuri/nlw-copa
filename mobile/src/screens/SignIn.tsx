51a15590e9be727f2d5d5f54ed04a8782c3b7795
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2022-11-21 20:34:00 -0300

    use hook screen

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,14 @@
 import { Center, Text, Icon } from 'native-base';
 import { Fontisto } from '@expo/vector-icons';
+import { useAuth } from '../hooks/useAuth';
 
 import Logo from '../assets/logo.svg';
 import { Button } from '../components/Button';
 
 export function SignIn() {
+
+    const { signIn } = useAuth();
+
     return (
         <Center flex={1} bgColor="gray.900" p={7}>
             <Logo width={212} height={40} />
@@ -14,6 +18,7 @@ export function SignIn() {
                 title="ENTRAR COM GOOGLE"
                 leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
                 mt={12}
+                onPress={signIn}
             />
 
             <Text
