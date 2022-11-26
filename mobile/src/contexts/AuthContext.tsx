5ee27f3ff91b0e4f937244aba266d452872e67b3
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2022-11-26 18:42:26 -0300

    complete authenticated with google

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState, useEffect } from 'react';
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
     name: string;
@@ -11,26 +16,49 @@ interface AuthProviderProps {
 
 export interface AuthContextDataProps {
     user: UserProps;
+    isUserLoading: boolean;
     signIn: () => Promise<void>;
 }
 
-export const AuthContext = createContext({
-
-} as AuthContextDataProps);
+export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+    const [user, setUser] = useState<UserProps>({} as UserProps);
+    const [isUserLoading, setIsUserLoading] = useState(false);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    });
 
     async function signIn() {
-        console.log('Vamos logar!');
+        try {
+            setIsUserLoading(true);
+            await promptAsync();
+        } catch (error) {
+            console.log(error);
+            throw error;
+        } finally {
+            setIsUserLoading(false);
+        }
     }
+
+    async function signInWithGoogle(acess_token: string) {
+        console.log("TOKE", acess_token);
+    }
+
+    useEffect(() => {
+        if (response?.type === 'success' && response?.authentication.accessToken) {
+            signInWithGoogle(response.authentication.accessToken);
+        }
+    }, [response]);
 
     return (
         <AuthContext.Provider value={{
             signIn,
-            user: {
-                name: 'Rodrigo',
-                avatarUrl: 'https://github.com/rodrigoyuri.png'
-            }
+            isUserLoading,
+            user
         }}>
             {children}
         </AuthContext.Provider>
