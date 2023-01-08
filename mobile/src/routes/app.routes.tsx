f98066aa0f22c1b38205be5ba37eee9fb8ba642f
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2023-01-08 14:35:27 -0300

    create new type and implemetation navigation

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { New } from '../screens/New';
 import { Pools } from '../screens/Pools';
+import { Find } from '../screens/Find';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -45,6 +46,12 @@ export function AppRoutes() {
                     tabBarIcon: ({ color }) => <SoccerBall color={color} size={size} />,
                     tabBarLabel: 'Meus bolões'
                 }}
+            />
+
+            <Screen
+                name="find"
+                component={Find}
+                options={{ tabBarButton: () => null }}
             />
         </Navigator>
     );
