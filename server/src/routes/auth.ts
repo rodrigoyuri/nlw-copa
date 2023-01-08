d32979407c317c57726b92ad4bd47abe87a1d41a
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2023-01-08 11:27:34 -0300

    plugin validation jwt and route profile

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function authRoutes(fastify: FastifyInstance) {
+    fastify.get('/me', { onRequest: [authenticate] },
+        async (request) => {
+            return { user: request.user };
+        });
+
     fastify.post('/users', async (request) => {
         const createUserBody = z.object({
             access_token: z.string(),
@@ -45,6 +51,14 @@ export async function authRoutes(fastify: FastifyInstance) {
             });
         }
 
-        return { userInfo };
+        const token = fastify.jwt.sign({
+            name: user.name,
+            avatarUrl: user.avatarUrl
+        }, {
+            sub: user.id,
+            expiresIn: '1 hour'
+        });
+
+        return { token };
     });
 }
